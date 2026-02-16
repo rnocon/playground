75a7abec294f958029c6b197e11f3ce0aee4f704
rnocon <nocon.rafal2004@gmail.com>
2026-02-16 15:53:43 +0000

    Backup geht restore nicht ganz

---
+++ testvm/skripts/confbackup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euo pipefail
+
+ENV_FILE='/workspaces/playground/testvm/backup.env'
+
+
+if [[ -f "$ENV_FILE" ]] ;
+then
+        export $(grep -v '^#' "$ENV_FILE" | xargs -0)
+else
+        echo "Warnung: $ENV_FILE nicht gefunden - Standardwerte werden genutzt."
+fi
+
+if [[ $# -lt 1 ]]; then
+  if [[ -n "${DEFAULT_BACKUP_PATH:-}" ]]; then
+    TARGET_PATH="$DEFAULT_BACKUP_PATH"
+    echo "Kein Pfad angegeben"
+    #exit 1
+  else
+    echo "Fehler: Kein Zielpfad angegeben und DEFAULT_BACKUP_PATH existiert nicht."
+    exit 1
+  fi
+else
+  TARGET_PATH="$DEFAULT_BACKUP_PATH/$1"
+fi
+
+cp /workspaces/playground/testvm/docker-compose.yml $TARGET_PATH
+
+cp '/workspaces/playground/testvm/backup.env' $TARGET_PATH
